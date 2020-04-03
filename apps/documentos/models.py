4c50e253de602911d115c9ff5978957ca5e25ca9
Samuel <Samuel Fernandes>
2020-04-03 12:21:58 -0300

    Relacinamento de banco de dados adicionado

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from apps.colaborador.models import Colaborador
 
 
 class Documeto(models.Model):
     descricao = models.CharField(max_length=100, help_text='Nome do Documeto')
+    pertence = models.ForeignKey(Colaborador, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from apps.colaborador.models import Colaborador
 
 
 class RegistroHora(models.Model):
     motivo = models.CharField(max_length=100, help_text='Informe o motivo')
+    colaborador = models.ForeignKey(Colaborador, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.motivo
